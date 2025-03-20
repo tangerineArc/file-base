1875dd1f1f4632cb17686a3206c2d7550fae18d2
Swagatam <swagatamheroz@outlook.com>
2025-03-20 22:10:37 +0530

    feat: create folder validator

---
+++ validators/folder-validator.js
@@ -0,0 +1,14 @@
+import { body } from "express-validator";
+
+const validateFolder = [
+  body("folderName")
+    .trim()
+    .isLength({ min: 1, max: 255 })
+    .withMessage(
+      "Folder name must be at least 1 character and at most 255 characters long"
+    )
+    .isAlphanumeric()
+    .withMessage("Folder name must only contain alphanumeric characters"),
+];
+
+export default validateFolder;
